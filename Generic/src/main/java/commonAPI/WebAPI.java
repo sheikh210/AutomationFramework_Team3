045b6522e7b26b96ecbc02f78e09879ee28e17ad
ssheikh <ssheikh210@gmail.com>
2020-06-04 20:47:31 -0400

    1st branch commit

--- Generic/src/main/java/commonAPI/WebAPI.java
+++ Generic/src/main/java/commonAPI/WebAPI.java
@@ -2,6 +2,7 @@ package commonAPI;
 
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.LogStatus;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.*;
@@ -81,7 +82,7 @@ public class WebAPI {
         ExtentTestManager.endTest();
         extent.flush();
         if (result.getStatus() == ITestResult.FAILURE) {
-            captureScreenshot(driver, result.getName());
+            captureScreenshot(driver);
         }
         driver.quit();
     }
@@ -100,7 +101,7 @@ public class WebAPI {
 
 
     //Browser SetUp
-    public static WebDriver driver = null;
+    public static WebDriver driver;
     public String browserstack_username = "mhshahib1";
     public String browserstack_accesskey = "YA4xsqrMqFurrGduX1X9";
     public String saucelabs_username = "";
@@ -110,8 +111,7 @@ public class WebAPI {
     @BeforeMethod
     public void setUp(@Optional("false") boolean useCloudEnv, @Optional("false") String cloudEnvName,
                       @Optional("windows") String os, @Optional("10") String os_version, @Optional("chrome-options") String browserName, @Optional("34")
-                              String browserVersion, @Optional("https://www.cnn.com") String url) throws IOException {
-        //System.setProperty("webdriver.chrome.driver", "/Users/peoplentech/eclipse-workspace-March2018/SeleniumProject1/driver/chromedriver");
+                              String browserVersion, @Optional("") String url) throws IOException {
         if (useCloudEnv == true) {
             if (cloudEnvName.equalsIgnoreCase("browserstack")) {
                 getCloudDriver(cloudEnvName, browserstack_username, browserstack_accesskey, os, os_version, browserName, browserVersion);
@@ -122,44 +122,40 @@ public class WebAPI {
             getLocalDriver(os, browserName);
         }
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        //driver.manage().timeouts().pageLoadTimeout(25, TimeUnit.SECONDS);
+        driver.manage().deleteAllCookies();
         driver.get(url);
-        //driver.manage().window().maximize();
+        driver.manage().window().maximize();
     }
 
     public WebDriver getLocalDriver(@Optional("mac") String OS, String browserName) {
 
-        if (browserName.equalsIgnoreCase("chrome")) {
-            if (OS.equalsIgnoreCase("OS X")) {
-                System.setProperty("webdriver.chrome.driver", "../Generic/BrowserDriver/mac/chromedriver");
-            } else if (OS.equalsIgnoreCase("Windows")) {
-                System.setProperty("webdriver.chrome.driver", "../Generic/BrowserDriver/windows/chromedriver.exe");
-            }
+        String windowsChromeDriverPath = "/Generic/lib/BrowserDrivers/Windows/chromedriver.exe";
+        String macChromeDriverPath = "/Generic/lib/BrowserDrivers/Mac/chromedriver";
+
+        if (browserName.equalsIgnoreCase("Chrome")) {
+            WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
+
+        } else if (browserName.equalsIgnoreCase("Firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+
+        } else if (browserName.equalsIgnoreCase("IE")) {
+            WebDriverManager.iedriver().setup();
+            driver = new InternetExplorerDriver();
+
         } else if (browserName.equalsIgnoreCase("chrome-options")) {
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--disable-notifications");
             if (OS.equalsIgnoreCase("OS X")) {
-                System.setProperty("webdriver.chrome.driver", "../Generic/BrowserDriver/mac/chromedriver");
+                System.setProperty("webdriver.chrome.driver", macChromeDriverPath);
             } else if (OS.equalsIgnoreCase("Windows")) {
-                System.setProperty("webdriver.chrome.driver", "../Generic/BrowserDriver/windows/chromedriver.exe");
+                System.setProperty("webdriver.chrome.driver", windowsChromeDriverPath);
             }
             driver = new ChromeDriver(options);
-        } else if (browserName.equalsIgnoreCase("firefox")) {
-            if (OS.equalsIgnoreCase("OS X")) {
-                System.setProperty("webdriver.gecko.driver", "../Generic/BrowserDriver/mac/geckodriver");
-            } else if (OS.equalsIgnoreCase("Windows")) {
-                System.setProperty("webdriver.gecko.driver", "../Generic/BrowserDriver/windows/geckodriver.exe");
-            }
-            driver = new FirefoxDriver();
-
-        } else if (browserName.equalsIgnoreCase("ie")) {
-            System.setProperty("webdriver.ie.driver", "../Generic/BrowserDriver/windows/IEDriverServer.exe");
-            driver = new InternetExplorerDriver();
         }
         return driver;
     }
-
 
     public WebDriver getCloudDriver(String envName, String envUsername, String envAccessKey, String os, String os_version, String browserName,
                                     String browserVersion) throws IOException {
@@ -182,7 +178,6 @@ public class WebAPI {
 
     @AfterMethod(alwaysRun = true)
     public void cleanUp() {
-        //driver.close();
         driver.quit();
     }
 
@@ -257,18 +252,16 @@ public class WebAPI {
         driver.navigate().back();
     }
 
-    public static void captureScreenshot(WebDriver driver, String screenshotName) {
-        DateFormat df = new SimpleDateFormat("(MM.dd.yyyy-HH:mma)");
+    public static void captureScreenshot(WebDriver driver) {
         Date date = new Date();
-        df.format(date);
+        String fileName = date.toString().replace(" ", "_").replace(":", "-") + ".png";
 
-        File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         try {
-            FileUtils.copyFile(file,
-                    new File(System.getProperty("user.dir") + "/Screenshots/" + screenshotName + " " + df.format(date) + ".png"));
-            System.out.println("Screenshot captured");
+            FileUtils.copyFile(screenshot, new File(System.getProperty("user.dir") + "\\lib\\Screenshots\\" + fileName));
+            System.out.println("SCREENSHOT TAKEN");
         } catch (Exception e) {
-            System.out.println("Exception while taking screenshot " + e.getMessage());
+            System.out.println("ERROR TAKING SCREENSHOT: " + e.getMessage());
         }
 
     }
@@ -279,14 +272,14 @@ public class WebAPI {
         return splitString;
     }
 
-    public static void clickOnElement(String locator, WebDriver driver1) {
+    public static void clickOnElement(String locator, WebDriver driver) {
         try {
-            driver1.findElement(By.cssSelector(locator)).click();
+            driver.findElement(By.cssSelector(locator)).click();
         } catch (Exception ex1) {
             try {
-                driver1.findElement(By.xpath(locator)).click();
+                driver.findElement(By.xpath(locator)).click();
             } catch (Exception ex2) {
-                driver1.findElement(By.id(locator)).click();
+                driver.findElement(By.id(locator)).click();
             }
         }
     }
@@ -366,15 +359,26 @@ public class WebAPI {
         return list;
     }
 
-    public List<WebElement> getListOfWebElementsByXpath(String locator) {
+    public List<WebElement> getListOfWebElementsByXpath(WebElement element, String locator) {
         List<WebElement> list = new ArrayList<WebElement>();
-        list = driver.findElements(By.xpath(locator));
+        list = element.findElements(By.xpath(locator));
+        return list;
+    }
+
+    public List<WebElement> getListOfWebElementsByTagName(WebElement element, String tagName) {
+        List<WebElement> list = new ArrayList<WebElement>();
+        list = element.findElements(By.tagName(tagName));
         return list;
     }
 
     public String getCurrentPageUrl() {
         String url = driver.getCurrentUrl();
         return url;
+    }
+
+    public String getCurrentPageTitle(){
+        String title = driver.getTitle();
+        return title;
     }
 
     public void navigateForward() {
@@ -427,24 +431,18 @@ public class WebAPI {
             WebElement element = driver.findElement(By.cssSelector(locator));
             Actions action = new Actions(driver);
             action.moveToElement(element).perform();
-
         }
-
     }
 
-    public void mouseHoverByXpath(String locator) {
+    public void mouseHover(WebElement element) {
         try {
-            WebElement element = driver.findElement(By.xpath(locator));
-            Actions action = new Actions(driver);
-            Actions hover = action.moveToElement(element);
+            Actions hover = new Actions(driver);
+            hover.moveToElement(element);
         } catch (Exception ex) {
-            System.out.println("First attempt has been done, This is second try");
-            WebElement element = driver.findElement(By.xpath(locator));
-            Actions action = new Actions(driver);
-            action.moveToElement(element).perform();
-
+            System.out.println("1st mouse-hover attempt failed - Attempting 2nd time");
+            Actions hover = new Actions(driver);
+            hover.moveToElement(element).perform();
         }
-
     }
 
     //handling Alert
@@ -470,12 +468,6 @@ public class WebAPI {
     //get Links
     public void getLinks(String locator) {
         driver.findElement(By.linkText(locator)).findElement(By.tagName("a")).getText();
-    }
-
-    //Taking Screen shots
-    public void takeScreenShot() throws IOException {
-        File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        //FileUtils.copyFile(file, new File("screenShots.png"));
     }
 
     //Synchronization
@@ -542,7 +534,7 @@ public class WebAPI {
     }
 
 
-    // Customer Made Helper Methods for Amex.com
+    // Custom-made Helper Methods for Amex.com
     public void brokenLink() throws IOException {
         //Step:1-->Get the list of all the links and images
         List<WebElement> linkslist = driver.findElements(By.tagName("a"));
--- espn/src/main/java/homepage/ESPNHomepage.java
+++ espn/src/main/java/homepage/ESPNHomepage.java
@@ -1,4 +1,113 @@
 package homepage;
 
-public class ESPNHomepage {
+import commonAPI.WebAPI;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
+
+import java.util.List;
+
+import static homepage.ESPNHomepageElements.*;
+
+public class ESPNHomepage extends WebAPI {
+
+    @FindBy(xpath = webElementTopEventsDropdown)
+    public WebElement topEventsDropdown;
+
+    @FindBy(css = webElementTopEventsDropdownMenu)
+    public WebElement topEventsDropdownMenu;
+
+    @FindBy(xpath = webElementFeaturedHoverBox)
+    public WebElement featuredHoverBox;
+
+    @FindBy(xpath = webElementFeaturedHoverHidden)
+    public WebElement featuredHoverHidden;
+
+    @FindBy(xpath = webElementNFLDropdown)
+    public WebElement nflDropdown;
+
+    @FindBy(xpath = webElementNBADropdown)
+    public WebElement nbaDropdown;
+
+    @FindBy(xpath = webElementMLBDropdown)
+    public WebElement mlbDropdown;
+
+    @FindBy(xpath = webElementNCAAFDropdown)
+    public WebElement ncaafDropdown;
+
+    @FindBy(xpath = webElementSoccerDropdown)
+    public WebElement soccerDropdown;
+
+    @FindBy(xpath = webElementMMADropdown)
+    public WebElement mmaDropdown;
+
+
+    /**
+     * Test Case 1 - Validate Navigation to Homepage
+     * 1 - Navigate to http://espn.com
+     * 2 - Capture page title and match with expected page title
+     */
+    @Override
+    public String getCurrentPageTitle() {
+        return super.getCurrentPageTitle();
+    }
+    public void validateCurrentPage(){
+        String actualTitle = getCurrentPageTitle();
+        String expectedTitle = "ESPN: Serving sports fans. Anytime. Anywhere.";
+
+        Assert.assertEquals(actualTitle, expectedTitle, "Homepage title does not match");
+    }
+
+    /**
+     * Test Case 2 - Validate number of elements in "Top Events" dropdown menu list
+     * 1 - Navigate to http://espn.com
+     * 2 - Click on "Top Events" dropdown menu
+     * 3 - Capture number of links present in "Top Events" dropdown menu
+     */
+    public int topEventsDropdownList(){
+        mouseHover(topEventsDropdown);
+        List<WebElement> topEventsDropDownList = getListOfWebElementsByXpath(topEventsDropdownMenu,"//*[@id=\"global-scoreboard\"]/div/div/div[1]/div[2]/ul/li/a");
+        int listSize = topEventsDropDownList.size();
+
+        return listSize;
+    }
+    public void validateTopEventsDropdownListSizeAndLinks(){
+        int actualListSize = topEventsDropdownList();
+        int expectedListSize = 8;
+
+        Assert.assertEquals(actualListSize, expectedListSize, "List size does not match");
+    }
+
+    /**
+     * Test Case 3 - Validate mouse-over changes "Featured" box and links to appropriate page
+     * 1 - Navigate to http://espn.com
+     * 2 - Hover over "Featured" hover box (located directly to the right)
+     * 3 - Validate box changes to display text "Full Coverage"
+     * 4 - Validate box links to appropriate page when clicked
+     */
+    public String featuredBoxHoverTextAndLink(){
+        String hiddenText;
+        String featuredBoxURL;
+        mouseHover(featuredHoverBox);
+        hiddenText=featuredHoverHidden.getText();
+        featuredBoxURL=featuredHoverHidden.getAttribute("innerHTML").trim();
+
+        return hiddenText+"\n"+featuredBoxURL;
+    }
+    public void validateFeaturedBox(){
+        Assert.assertEquals(featuredBoxHoverTextAndLink(), "Full Coverage\nhttps://fantasy.espn.com/games/ufc-250-pickem/make-picks?addata=ufc250pickem_ufc_web_headerscoreboard_p1");
+    }
+
+
+
+    /**
+     * Test Case  - NFL Dropdown Menu (Left Div)
+     *      * 1 - Navigate to http://espn.com
+     *      * 2 - Hover over NFL Dropdown on header bar
+     *      * 3 - Ensure there are 9 links located on left side of dropdown menu (Home, Scores, Schedule, Teams, Standings,
+     *      *     Stats, Draft, Depth Charts, Free Agency), and capture links
+     */
+
 }
